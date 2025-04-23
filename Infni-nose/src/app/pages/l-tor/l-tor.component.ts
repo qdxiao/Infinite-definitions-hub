34c5ef3807f708b1f762fc2826dbfc888be6463f
zhangjunjie <2785478735@qq.com>
2025-04-23 17:55:21 +0800

    feat(src): 增加登录页面

--- Infni-nose/src/app/pages/l-tor/l-tor.component.ts
+++ Infni-nose/src/app/pages/l-tor/l-tor.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-l-tor',
   imports: [
-    NzCardModule
+    NzCardModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzFormModule,
+    NzInputModule,
+    NzSelectModule,
+    NzDividerModule,
+    NzIconModule,
+    NzGridModule,
   ],
   templateUrl: './l-tor.component.html',
   styleUrl: './l-tor.component.less'
@@ -12,4 +34,25 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 export class LTorComponent {
   title="Infni Community";
   subtitle="探索无限可能得星球"
+
+  private login = inject(NonNullableFormBuilder);
+  validateLoginForm = this.login.group({
+    email: this.login.control('', [Validators.email, Validators.required]),
+    password: this.login.control('', [Validators.required]),
+  });
+
+
+  submitForm(): void {
+    if (this.validateLoginForm.valid) {
+      console.log('submit', this.validateLoginForm.value);
+    } else {
+      Object.values(this.validateLoginForm.controls).forEach(control => {
+        console.log('control', control);
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
